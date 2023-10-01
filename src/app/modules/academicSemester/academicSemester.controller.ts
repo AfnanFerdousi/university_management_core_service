bb081b5a7a962dec66c19269a8920b8d5d05ee37
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-10-01 20:40:37 +0600

    [UPDATED] used catchAsync

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,23 +1,19 @@
-import { NextFunction, Request, Response } from "express";
-import { AcademicSemesterService } from "./academicSemester.service";
-import sendResponse from "../../../shared/sendResponse";
+import { Request, Response } from 'express';
+import { AcademicSemesterService } from './academicSemester.service';
+import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemester } from '@prisma/client';
+import catchAsync from '../../../shared/catchAsync';
 
-const insertIntoDB = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const result = await AcademicSemesterService.insertIntoDB(req.body);
-        sendResponse<AcademicSemester>(res,{
-            statusCode: 200,
-            success: true,
-            message: "inserted",
-            data: result
-        })
-    } catch (error) {
-        next(error);        
-    }
-
-}
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: 200,
+    success: true,
+    message: 'inserted',
+    data: result,
+  });
+});
 
 export const AcademicSemesterController = {
-    insertIntoDB
-}
+  insertIntoDB,
+};
